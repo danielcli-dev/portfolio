73753ee28636846849ea9adac80e35a383c201d0
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2024-01-30 10:59:49 -0800

    Updated projectList images

--- src/projectList.js
+++ src/projectList.js
@@ -17,7 +17,7 @@ export const projectList = [
     url: "https://danielcli-dev.github.io/meet/",
     github: "https://github.com/danielcli-dev/meet",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/meet_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/meet_screenshot.png?raw=true",
     name: "Meet",
     description: `The app is used to display a list of upcoming events by
       CareerFoundry. Within this project, I was responsible for the
@@ -42,7 +42,7 @@ export const projectList = [
     github: "https://github.com/danielcli-dev/shopaholic",
     casestudy: "https://danielcli.com/case-study/shopaholic",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/shopaholic_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/shopaholic_screenshot.png?raw=true",
     name: "Shopaholic",
     description: `Simple e-commerce website for selling clothing and accessories. Allows for users to login with shopping basket that retains added items and a orders list for previous purchases. It also has search, filter and sort functionality. Using stripe, fake orders can be placed at checkout screen. Within this project, I was responsible for the frontend and backend.
       It’s written in React.js.  Also, I used Google Firestore to save data and used
@@ -71,7 +71,7 @@ export const projectList = [
     github: "https://github.com/danielcli-dev/spoileralert",
     casestudy: "https://danielcli.com/case-study/spoileralert",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/spoileralert_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/spoileralert_screenshot.png?raw=true",
     name: "Spoiler Alert",
     description: `Web app is used to search for your favorite movies and find the plot of the movie.
       It’s written in React.js and styled using CSS. Also, I used Axios to make API requests to the TMDB
@@ -99,7 +99,7 @@ export const projectList = [
     url: "https://twitter-clone-6b4c7.web.app/",
     github: "https://github.com/danielcli-dev/twitter-clone",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/twitter_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/twitter_screenshot.png?raw=true",
     name: "Twitter Clone",
     description: `Twitter clone with user login and allow for posting messages to shared wall. Only the creator of the message can edit or delete message. Google Authentication is used to create a
       user sign-in and the Firestore Database for storing messages.`,
@@ -126,7 +126,7 @@ export const projectList = [
     url: "https://whatsapp-1da63.web.app/",
     github: "https://github.com/danielcli-dev/whatsapp-clone",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/whatsapp_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/whatsapp_screenshot.png?raw=true",
     name: "What's App Clone",
     description: `Whats App clone which functionality to create unique chatrooms that allow messages from different users. Messages display user's own messages on the right in green and other users' messages on the left in white. The app uses Google Auth to allow users to sign in using their Google profile. Also, I have used Material-UI icons to design the
       website, as well as, used Firestore Database to save chat rooms and
@@ -155,7 +155,7 @@ export const projectList = [
     url: "https://danielcli-dev.github.io/pokedex-project/",
     github: "https://github.com/danielcli-dev/pokedex-project",
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/pokedex-project_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/pokedex-project_screenshot.png?raw=true",
     name: "Pokedex",
     description: `Website contains list of Pokemon which can be filtered alphabetically and by their Index #. When selecting a Pokemon, a modal appears containing an image and additional information pertaining to that specific Pokemon. All data is retrieved from the Pokdex API. Within this project, I was responsible for the frontend.
       It’s written using HTML, CSS and jQuery.`,
@@ -200,7 +200,7 @@ export const projectList = [
     name: "myFlix",
     description: `myFlix is a web app containing information pertaining to movies. It allows users to sign in and save their favorite movies.`,
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/myflix_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/myflix_screenshot.png?raw=true",
     tech: [
       {
         name: "React",
@@ -218,7 +218,7 @@ export const projectList = [
     name: "myFlix-Angular",
     description: `myFlix is a web app containing information pertaining to movies. It allows users to sign in and save their favorite movies.`,
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/myFlix-Angular_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/myFlix-Angular_screenshot.png?raw=true",
     tech: [
       {
         name: "Angular",
@@ -235,12 +235,12 @@ export const projectList = [
     ],
   },
   {
-    github: "https://github.com/danielcli-dev/myFlix-Angular-client",
+    github: "https://github.com/danielcli-dev/chat",
     name: "Chat",
     casestudy: "https://danielcli.com/case-study/chat",
     description: `Chat is a React Native App that allows users to sign in and chat with each other. It also supports geolocation, photos, and audio messages.`,
     image:
-      "https://github.com/danielcli-dev/website-portfolio/blob/master/img/chat_screenshot.png?raw=true",
+      "https://github.com/danielcli-dev/portfolio/blob/main/public/chat_screenshot.png?raw=true",
     tech: [
       {
         name: "React Native",
