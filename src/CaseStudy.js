b085fea9f158f3af2fc326b547fc19fcf3546515
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2024-03-07 15:32:50 -0800

    Added missing imports

--- src/CaseStudy.js
+++ src/CaseStudy.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useParams } from "react";
 import "./CaseStudy.css";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 function CaseStudy() {
   const { projectName } = useParams();
